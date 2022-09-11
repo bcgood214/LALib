83ad8caaa12099da5f8557b7191c3138d51ce350
bcgood <bengood98@gmail.com>
2022-09-11 13:03:53 -0500

    First commit

---
+++ matrix_matrix_multiplication.py
@@ -0,0 +1,21 @@
+def mult(a, b):
+    if len(a[0]) != len(b):
+        return False
+    
+    c = [ [0]*(len(b[0])) for i in range(len(a))]
+    print(len(c))
+    print(len(c[0]))
+
+    for i in range(len(b[0])):
+        for j in range(len(a)):
+            v = 0
+            for k in range(len(a[0])):
+                v += a[j][k] * b[k][i]
+            c[j][i] = v
+
+    return c
+
+if __name__ == "__main__":
+    a = [[2, -1, 1, -1], [0, 1, 3, 1], [1, 0, 1, 1]]
+    b = [[2, 0, 1], [1, 1, 0], [2, 0, 1], [1, 1, 0]]
+    print(mult(b, a))

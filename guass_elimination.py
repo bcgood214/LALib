182c166da8f3f7bd9ba02e90a3bf0045e522c916
bcgood <bengood98@gmail.com>
2022-09-12 19:21:56 -0500

    row replacement

--- guass_elimination.py
+++ guass_elimination.py
@@ -2,6 +2,11 @@ def gauss_elimination(mat):
     row_ind = 0
     for i in range(len(mat)-1):
         for j in range(i+1, len(mat)):
+            if mat[i][row_ind] == 0:
+                if i+1 <= len(mat)-1:
+                    temp = mat[i]
+                    mat[i] = mat[i+1]
+                    mat[i+1] = temp
             mat[j][row_ind] = mat[j][row_ind]/mat[i][row_ind]
             for k in range(row_ind+1, len(mat[0])):
                 mat[j][k] = mat[j][k] - (mat[j][row_ind]*mat[i][k])
@@ -17,6 +22,9 @@ def forward_sub(mat, res):
     
     return res
 
+def back_sub(mat, res):
+    pass
+
 if __name__ == "__main__":
     m = [
         [2, 0, 1, 2],
@@ -26,10 +34,11 @@ if __name__ == "__main__":
     ]
     m2 = [
         [2, 4, -2],
-        [4, -2, 6],
+        [4, 8, 6],
         [6, -4, 2]
     ]
 
     m2 = gauss_elimination(m2)
+    print(m2)
     res = [-10, 20, 18]
     print(forward_sub(m2, res))
